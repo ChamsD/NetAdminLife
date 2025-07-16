e71e978792e7b5eccda6828885ad56e53ff37dd1
ChamsD <kokme.km@gmail.com>
2025-07-16 22:28:12 +0700

    styling menu card user, datatables card user, linked dashboard

---
+++ NetAdminLte/NetAdminLte/Controllers/CardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetAdminLte.Controllers;
+
+[Route("Card", Name ="Card")]
+public class CardController : Controller
+{
+    [Route("CardUser", Name ="CardUser")]
+    public IActionResult Index()
+    {
+        return View("~/Pages/Card/Index.cshtml");
+    }
+}
+
